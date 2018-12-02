286291a5d3f2192fd8a148a6bee181774f156a54
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2018-12-01 20:30:56 -0600

    a

--- Nihal/main.py
+++ Nihal/main.py
@@ -10,3 +10,18 @@ print(
      |       |      |       |___       _|_    _|_  _\|_   _|_  _\|_    \_____/    \_____    |___    _|_   _\|_       |
 ------------------------------------------------------------------------------------------------------------------------
 ''')
+name = str(input('What is your name? '))
+print('D A Y   1')
+x = input('...')
+del x
+x = input ('BEEEEP     BEEEEEEP      BEEEEEEP')
+del x
+print("''Ugh, the alarm. Time to get up, I guess.''")
+print('To make a choice, use the number next to the choice.')
+done = 0
+while not done:
+	try:
+		c = int(input('1. Get out of bed 2. Stay in bed'))
+		done = 1
+	except:
+		print('Please use the numbers.')
